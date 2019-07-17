9223bc97a0d274e3630de812a64954cf66b4d1be
Michael Bausano <michael@joinpouch.com>
2019-07-17 20:38:16 +0100

    Locking the hash map and inserting new urls

--- src/scraper.rs
+++ src/scraper.rs
@@ -9,7 +9,7 @@ type Database = Arc<Mutex<HashMap<String, HashSet<String>>>>;
 /// It checks the domain and makes a request to it. It collects recursivelly all urls it can
 /// find that belong to the same hostname. These urls are then stored in a HashSet and commited
 /// to the database.
-pub fn listen(_: Database, consumer: Receiver<String>) {
+pub fn listen(db: Database, consumer: Receiver<String>) {
     loop {
         // Waits for a message to be sent down the channel.
         let message = consumer.recv();
@@ -26,16 +26,42 @@ pub fn listen(_: Database, consumer: Receiver<String>) {
         match Url::parse(&message.unwrap()).ok().filter(|url| url.has_host()) {
             Some(url) => {
                 // Unwrap here is safe as we have filtered `has_host` in match statement.
-                scrape_urls(url.as_str(), url.host_str().unwrap())
+                scrape_urls(&db, url.as_str(), url.host_str().unwrap());
             },
             None => continue,
         }
-
-        // TODO: Extract hostname from url.
-        // TODO: Scrape all urls on given domain belonging to hostname.
     }
 }
 
-fn scrape_urls(url: &str, host: &str) {
+fn scrape_urls(master: &Database, url: &str, host: &str) {
     println!("[{}] Scraping {}", host, url);
+
+    let scraped: HashSet<String> = HashSet::new();
+
+    let _diff: Vec<String> = {
+        let mut map = master.lock().unwrap();
+
+        match map.get_mut(host) {
+            Some(set) => {
+                let difference: Vec<String> = scraped.difference(set).cloned().collect();
+                let mut items: Vec<String> = Vec::new();
+
+                for url in difference {
+                    (*set).insert(url.clone());
+                    items.push(url.to_string());
+                }
+
+                items
+            },
+            None => {
+                let items: Vec<String> = scraped.iter().cloned().collect();
+
+                map.insert(host.to_string(), scraped);
+
+                items
+            }
+        }
+    };
 }
+
+
